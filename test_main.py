542c4e7b8868915d8699ee1c5bf28d0e18e83ef0
protopych <94621777+protopych@users.noreply.github.com>
2022-12-24 10:02:39 +0300

    Add tests

---
+++ test_main.py
@@ -0,0 +1,23 @@
+import pytest
+from main import random_int_array, qsort, bubble_sort, merge_sort
+
+
+def test_buble_sort():
+    arr = random_int_array()
+    arr1 = arr[:]
+    bubble_sort(arr1)
+    assert arr1 == sorted(arr)
+
+
+def test_qsort():
+    arr = random_int_array()
+    arr1 = arr[:]
+    qsort(arr1)
+    assert arr1 == sorted(arr)
+
+
+def test_merge_sort():
+    arr = random_int_array()
+    arr1 = arr[:]
+    merge_sort(arr1)
+    assert arr1 == sorted(arr)
